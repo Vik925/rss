6011f32cc9f93f49706b53cbe28f18e684599f40
Viktoriy Abdykhanova <vikylya917@gmail.com>
2020-07-26 22:17:13 +0300

    add

--- rss.js
+++ rss.js
@@ -1,12 +1,3 @@
-/*function addScript(src){
-  var script = document.createElement('script');
-  script.src = src;
-  script.async = false; // чтобы гарантировать порядок
-  document.head.appendChild(script);
-}
-
-addScript('https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.17.1/parser.js'); */
-
 async function srr(){
 
     const cors = 'https://cors-anywhere.herokuapp.com';
@@ -24,39 +15,33 @@ async function srr(){
         const xmlTxt = await response.text();
       //  console.log('xmlTxt', xmlTxt);
 
-      var jsonObj = parser.parse(xmlTxt, [options] );
-      var parser = require('fast-xml-parser');
-      var he = require('he');
-       
-      var options = {
-          attributeNamePrefix : "@_",
-          attrNodeName: "attr", //default is 'false'
-          textNodeName : "#text",
-          ignoreAttributes : true,
-          ignoreNameSpace : false,
-          allowBooleanAttributes : false,
-          parseNodeValue : true,
-          parseAttributeValue : false,
-          trimValues: true,
-          cdataTagName: "__cdata", //default is 'false'
-          cdataPositionChar: "\\c",
-          parseTrueNumberOnly: false,
-          arrayMode: false, //"strict"
-          attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
-          tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
-          stopNodes: ["parse-me-as-string"]
-      };
-       
-      if( parser.validate(xmlData) === true) { //optional (it'll return an object in case it's not valid)
-          var jsonObj = parser.parse(xmlData,options);
-      }
-       
-      // Intermediate obj
-      var tObj = parser.getTraversalObj(xmlData,options);
-      var jsonObj = parser.convertToJson(tObj,options);
-       
-    
-  
+      
+      const parser = new DOMParser();
+      const doc = parser.parseFromString.bind(xmlTxt,"text/xml");
+      
+
+     // const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser());
+        
+
+       // let doc = DOMPARSER(xmlTxt, "text/xml");
+       console.log('doc', doc);
+
+        let heading = document.createElement('h1');
+        heading.textContent = url.hostname;
+
+        const frag = document.createDocumentFragment();
+        frag.appendChild(heading);
+
+        doc.querySelectorAll('item').forEach((item) => {
+            let temp = document.importNode(document.querySelector('template').content, true);
+            let i = item.querySelector.bind(item)
+            let t = temp.querySelector.bind(temp)
+            t('h2').textContent = !!i('title') ? i('title').textContent : '-'
+            t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#'
+            t('p').innerHTML = !!i('description') ? i('description').textContent : '-'
+            t('h3').textContent = url.hostname
+            frag.appendChild(temp) 
+        });
       
  };
 
