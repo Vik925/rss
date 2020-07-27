e7787019e1eeed6bce5e4a261089606aade0d585
Viktoriy Abdykhanova <vikylya917@gmail.com>
2020-07-27 16:55:31 +0300

    add

--- rss.js
+++ rss.js
@@ -1,13 +1,14 @@
-import parser from 'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.17.1/parser.js';
+//import parser from 'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.17.1/parser.js';
+function srr(){
 
-  
+
+
   const cors = 'https://cors-anywhere.herokuapp.com';
-  const url = 'https://www.hongkiat.com/blog/feed/';
+  const url = new URL ('https://www.hongkiat.com/blog/feed/');
 
   let heading = document.querySelector('h1');
-    heading.textContent = url;
+    heading.textContent = url.host;
 
-   
 
   async function fetching(){
     
@@ -19,8 +20,33 @@ import parser from 'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.17.
     });
     
     const xmlTxt = await response.text();
-  
-    const options = {
+
+    parser = new DOMParser();
+    docXml = parser.parseFromString(xmlTxt,"application/xml");
+    
+  //  document.querySelector("h2").innerHTML =
+   // docXml.getElementsByTagName("title")[0].childNodes[0].nodeValue; 
+
+    const frag = document.createDocumentFragment();
+      frag.appendChild(heading);
+    
+     docXml.querySelectorAll('item')
+    .forEach((item) => {
+   
+      let temp = document.importNode(document.querySelector('template').content, true);
+      let i = item.querySelector.bind(item);
+      let t = temp.querySelector.bind(temp);
+
+      t('h2').textContent = !!i('title') ? i('title').textContent : '-';
+      t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
+      t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
+      t('h3').textContent = url.host;
+      frag.appendChild(temp); 
+    });
+
+  };
+  fetching();
+   /* const options = {
     attributeNamePrefix: '@_',
     attrNodeName: 'attr', //default is 'false'
     textNodeName: '#text',
@@ -41,14 +67,13 @@ import parser from 'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.17.
   const jsonObj = await parser.parse(xmlTxt, options);
 
   const result = jsonObj;
-  return result;
+  return result;*/
 
   /*  const frag = document.createDocumentFragment();
     frag.appendChild(heading);
     //console.log(frag);
 
-    parser = new DOMParser();
-    doc = parser.parseFromString(xmlTxt,"text/xml");
+   
 
    doc.querySelectorAll('item').forEach((item) => {
     let temp = document.importNode(document.querySelector('template').content, true);
@@ -60,7 +85,4 @@ import parser from 'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.17.
     t('h3').textContent = url;
     frag.appendChild(temp); 
 }); */
-  
 };
-
-fetching();
